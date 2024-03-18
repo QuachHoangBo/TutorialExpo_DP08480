a5a64cf75b19e0481f6e9370adb150abf7dbd0a3
QuachHoangBo <121910981+QuachHoangBo@users.noreply.github.com>
2024-03-18 17:01:11 +0700

    Revert "reload file"
    This reverts commit 190c5e2aefd2d1360d3df624c71fb29c9d411544.

---
+++ src/homework/lab1/components/InputLab1-3.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+const { View, Text, TextInput } = require("react-native");
+
+const InputLab1_3 = ({ title }) => {
+  const [inputText, setInputText] = React.useState("");
+  return (
+    <View style={{ marginHorizontal: 20 }}>
+      <Text style={{ fontWeight: "bold" }}>{title} </Text>
+      <TextInput
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          marginVertical: 10,
+          width: 200,
+        }}
+        value={inputText}
+        onChangeText={(text) => setInputText(text)}
+      />
+    </View>
+  );
+};
+
+InputLab1_3.propTypes = {
+  title: PropTypes.string,
+};
+
+export default InputLab1_3;
