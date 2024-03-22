9ad42c34f8fe1c12d4a4223a31659cbca0996fe6
QuachHoangBo <121910981+QuachHoangBo@users.noreply.github.com>
2024-03-22 10:59:32 +0700

    Revert "learning"
    This reverts commit 28cfd85f90d648899a5ff3c993d725a27a0b723b.

--- src/Studying/Component/File1.js
+++ src/Studying/Component/File1.js
@@ -1,30 +1,8 @@
-import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const Component = () => {
-  const [count, setcount] = useState(0);
-  return (
-    <View style={styles.container}>
-      <Text>{count}</Text>
-      <Text
-        onPress={() => {
-          setcount(count + 1);
-          console.log("click");
-        }}
-      >
-        Click
-      </Text>
-    </View>
-  );
+  <View>
+    <Text>Hello</Text>
+  </View>;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-  },
-});
 export default Component;
--- src/Studying/Run.js
+++ src/Studying/Run.js
@@ -1,21 +1,14 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import File1 from "./Component/File1";
 
-const Run = () => {
-  return (
-    <View style={styles.container}>
-      <File1 />
-    </View>
-  );
+const App = () => {
+  <View>
+    <File1 />
+    <File1 />
+    <File1 />
+    <File1 />
+    <File1 />
+  </View>;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-export default Run;
+export default App;
