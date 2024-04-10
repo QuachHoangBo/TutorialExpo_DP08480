1d8c73371f22c39c10145dee808eb6680252b0f7
QuachHoangBo <121910981+QuachHoangBo@users.noreply.github.com>
2024-04-10 10:41:06 +0700

    working

--- src/Homework/components/Utils/until.tsx
+++
@@ -1,4 +0,0 @@
-import {Dimensions} from 'react-native';
-
-export const {width: WINDOW_WIDTH, height: WINDOW_HEIGHT} =
-  Dimensions.get('window');
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.js/**/*.{js,ts,jsx,tsx}",
+    "./src/Homework/Assignment/screens/**/*.{js,ts,jsx,tsx}",
+    "./src/Homework/Assignment/Components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
